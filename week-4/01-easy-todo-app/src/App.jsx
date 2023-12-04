5bb6b74b245b6bc8adf7cd9ee5a1d464d4b502fa
Manjunath Patil <btcpatil11@gmail.com>
2023-12-04 18:57:38 +0530

    easy react app

--- week-4/01-easy-todo-app/src/App.jsx
+++ week-4/01-easy-todo-app/src/App.jsx
@@ -1,27 +1,48 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [todos, setTodos] = useState([])
-    // fetch all todos from server
+  // fetch all todos from server
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/todos")
+      .then((response) => {
+        console.log(response.data)
+        setTodos(response.data);
+      })
+  }, [])
 
   return (
     <>
-      <div>
-        <h1>Easy Todo App</h1>
-        <input type="text" />
-      </div>
+      <Todo todos={todos} />
     </>
   )
 }
 
 function Todo(props) {
-    // Add a delete button here so user can delete a TODO.
-    return <div>
-        {props.title}
-    </div>
+
+  function deleteTodo(id) {
+    axios.delete(`http://localhost:3000/todos/${id}`)
+         .then(response => {
+          console.log("Todo deleted successfully :" + response) 
+         })
+  }
+  // Add a delete button here so user can delete a TODO.
+  return <div>
+    {props.todos.map(todo => (
+      <div>
+        {todo.title}
+        <br />
+        {todo.description}
+        <button onClick={deleteTodo(todo.id)} >Delete</button>
+      </div>
+    ))}
+
+  </div>
 }
 
 export default App
